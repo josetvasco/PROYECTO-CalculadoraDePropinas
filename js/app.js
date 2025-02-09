2ec6c32374845ad70ba1c3e9af6a9e074468281f
josetvasco <torovascojosedaniel@gmail.com>
2025-02-09 13:43:11 -0500

    Validando los campos del formulario, mostrando su alerta del llenado y cerrando el formulario

---
+++ js/app.js
@@ -0,0 +1,44 @@
+let cliente = {
+  mesa : "",
+  hora: "",
+  pedido: [] 
+};
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente() {
+  const mesa = document.querySelector('#mesa').value;
+  const hora = document.querySelector('#hora').value;
+
+
+  //Revisar si hay campos vacíos
+  const camposVacios = [ mesa, hora ].some( campo => campo === '' );
+
+  if( camposVacios ) {
+    //Verificar si hay alerta
+    const existeAlerta = document.querySelector('.invalid-feedback');
+
+    if(!existeAlerta) {
+      const alerta = document.createElement('DIV');
+      alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+      alerta.textContent = 'Todos los campos son obligatorios';
+      document.querySelector('.modal-body form').appendChild(alerta);
+
+      setTimeout(() => {
+        alerta.remove();
+      }, 3000);
+    }
+
+    return;
+  }
+
+  //Asiganr datos al objeto de cliente  
+  cliente = { ...cliente, mesa, hora };
+
+  //Ocultar modal
+  const modalFormulario = document.querySelector('#formulario');
+  const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+
+  modalBootstrap.hide();
+}
