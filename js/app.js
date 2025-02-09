9e23258440e3fb2798714601a0930d7a1aa7e61a
josetvasco <torovascojosedaniel@gmail.com>
2025-02-09 15:13:06 -0500

    Mostrando las secciones ocultas de los platillos y resumen de consumo

--- js/app.js
+++ js/app.js
@@ -41,4 +41,12 @@ function guardarCliente() {
   const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
 
   modalBootstrap.hide();
+
+  //Mostrar la secciones
+  mostrarSecciones(); 
+}
+
+function mostrarSecciones() {
+  const seccionesOcultas = document.querySelectorAll('.d-none');
+  seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
 }
